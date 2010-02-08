f6681535b54680e3203309ebc587029669bb1ac4
Peinthor Rene <peinthor@gmail.com>
2010-02-08 10:32:12 +0100

    code cleanup and improvements

--- src/com/oldsch00l/libHoN/PlayerStats.scala
+++ src/com/oldsch00l/libHoN/PlayerStats.scala
@@ -1,13 +1,15 @@
 package com.oldsch00l.libHoN
 
 class PlayerStats( xmlData: scala.xml.Elem) {
-	def attribute( name: String) = {
-	  xmlData \\ "stat" filter attributeValueEquals( name);
-	}
+  def attribute( name: String) : String = {
+    (xmlData \ "player_stats" \ "stat").filter( attributeValueEquals( name)).text
+  }
  
- 	def attributeValueEquals(value: String)(node: scala.xml.Node) = {
-	     node.attributes.exists(_.value == value)
-	}
+  def attributeValueEquals(value: String)(node: scala.xml.Node) = {
+     node.attributes.exists(_.value == value)
+  }
 
-	override def toString =   xmlData.toString
+  def getAID : String = (xmlData \ "player_stats" \ "@aid").text;
+
+  override def toString =   xmlData.toString
 }
