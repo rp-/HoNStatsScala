7bf5a1df607888fb39d685e6fd524be0bceb2eed
Peinthor Rene <rene.peinthor@altova.com>
2010-02-15 15:55:12 +0100

    use Option[] for return

--- src/com/oldsch00l/libHoN/libHoNTester.scala
+++ src/com/oldsch00l/libHoN/libHoNTester.scala
@@ -1,14 +1,16 @@
 package com.oldsch00l.libHoN;
 
 object Tester {
-	def main(args : Array[String]) : Unit = {
-		val erpe = StatsFactory.getPlayerStats( "erpe")
-		val wins = erpe.attribute( StatsFactory.PlayerAttr.WINS);
-        val aid = erpe.getAID;
+  def main(args : Array[String]) : Unit = {
+    val erpe = StatsFactory.getPlayerStats( 342)
+    if( erpe != None) {
+      val wins = erpe.get.attribute( PlayerAttr.WINS);
+      val aid = erpe.get.getAID;
 
-        println( erpe );
-		println( wins );
-        println( erpe.attribute( StatsFactory.PlayerAttr.GAMES_PLAYED) );
-        println( aid )
-  	}
+      println( erpe.get );
+      println( wins );
+      println( erpe.get.attribute( PlayerAttr.GAMES_PLAYED) );
+      println( aid )
+    }
+  }
 }
