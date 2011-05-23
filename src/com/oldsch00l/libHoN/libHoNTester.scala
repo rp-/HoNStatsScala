0da75cac9151429134318ead3e548cd5c6093e9d
Peinthor Rene <rene.peinthor@altova.com>
2011-05-23 15:13:11 +0200

    more debug output for the main

--- src/com/oldsch00l/libHoN/libHoNTester.scala
+++ src/com/oldsch00l/libHoN/libHoNTester.scala
@@ -1,14 +1,22 @@
 package com.oldsch00l.libHoN;
 
-
 object Tester extends App {
   //val erpe = StatsFactory.getPlayerStatsByAid( (2500 to 2600).toList)
   val pllist = StatsFactory.getPlayerStatsByNick(List("Erpe", "Sandla"));
 
-  val erpe: PlayerStats = pllist(0);
-//  val matches = erpe.getPlayedMatches
+  val erpe: PlayerStats = pllist(0)
+  val marc: PlayerStats = pllist(1)
+  //  val matches = erpe.getPlayedMatches
   //println(matches.length)
-  pllist.foreach(p => println(p.attribute(PlayerAttr.NICKNAME) + "(" + p.getAID + ") = PSR: " + p.attribute(PlayerAttr.RANK_AMM_TEAM_RATING) + " played: " + p.getPlayedMatches.size) )
+  val rpTime = System.currentTimeMillis
+  val rpmatches = erpe.getPlayedMatches
+  println("Erpe fetch(" + rpmatches.size + ") time: " + (System.currentTimeMillis - rpTime))
+
+  val marcTime = System.currentTimeMillis
+  val marcmatches = marc.getPlayedMatches
+  println("Marc fetch(" + marcmatches.size + ") time: " + (System.currentTimeMillis - marcTime))
+
+  pllist.foreach(p => println(p.attribute(PlayerAttr.NICKNAME) + "(" + p.getAID + ") = MMR: " + p.attribute(PlayerAttr.RANK_AMM_TEAM_RATING) + " played: " + p.getPlayedMatches.size))
   println("list size: " + pllist.length)
 
   /*    if( erpe != Nil) {
