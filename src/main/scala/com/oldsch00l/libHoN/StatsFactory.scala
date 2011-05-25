96f2f001419579736daefe62b1a8662e869e289f
Peinthor Rene <rene.peinthor@altova.com>
2011-05-25 15:25:47 +0200

    concurrent match fetching

--- src/main/scala/com/oldsch00l/libHoN/StatsFactory.scala
+++ src/main/scala/com/oldsch00l/libHoN/StatsFactory.scala
@@ -1,14 +1,17 @@
 package com.oldsch00l.libHoN
 
 import scala.xml._;
+import scala.actors.Actor
+import scala.actors.Actor._
 import java.sql.{ Connection, DriverManager, ResultSet };
 
-object StatsFactory {
+object StatsFactory extends Actor {
   val XMLRequester = "http://xml.heroesofnewerth.com/xml_requester.php"
   //load db driver
   Class.forName("org.hsqldb.jdbc.JDBCDriver").newInstance
   val connection = DriverManager.getConnection("jdbc:hsqldb:file:statsdb", "SA", "")
   MatchStatsSql.createTable(connection)
+  start
 
   def getPlayerStatsByNick(nicks: List[String]): List[PlayerStats] = {
     val query = nicks.mkString("&nick[]=");
@@ -16,7 +19,7 @@ object StatsFactory {
     if (xmlData.label == "error")
       Nil
     else {
-      val ret = (for { player <- (xmlData \\ "player_stats") } yield new PlayerStats(player)).toList;
+      val ret = (for { player <- (xmlData \ "stats" \ "player_stats") } yield new PlayerStats(player)).toList;
 
       if (nicks.length > 50)
         ret ::: getPlayerStatsByNick(nicks.drop(50))
@@ -46,24 +49,97 @@ object StatsFactory {
 
     val cached = MatchStatsSql.getEntries(connection, ids)
     val fetchids = ids filterNot ((for { c <- cached } yield c.getMatchID) contains)
-    return cached ::: fetchMachStats(fetchids)
+
+    m_results = Nil
+    if (fetchids.size > 0) {
+      println("tofetch: " + fetchids.size)
+      val workers = for (n <- 0 until 10) yield new WorkerActor
+
+      inProgress = workers.size;
+      workers.foreach(w => w.start)
+
+      var curWorker = 0
+      for (id <- fetchids) {
+        workers(curWorker) ! QueryArgs(XMLRequester + "?f=match_stats&opt=mid&mid[]=" + id)
+        curWorker += 1
+        if (curWorker == workers.size)
+          curWorker = 0
+      }
+
+      workers.foreach(w => w ! "STOP")
+
+      while (inProgress > 0) Thread.sleep(10)
+    }
+
+    val res = m_results
+    //val parfetchids = fetchids.par
+    //val res = parfetchids.map(id => fetchMachStats(id)).toList
+    res.foreach(m => m.cacheEntry(connection))
+    return cached ::: res
   }
 
-  private def fetchMachStats(ids: List[Int]): List[MatchStats] = {
+  private def fetchMatchStats(id: Int): MatchStats = {
+    val xmlData = XML.load(XMLRequester + "?f=match_stats&opt=mid&mid[]=" + id)
+    val match_ = (xmlData \ "stats" \ "match")
+    println((match_ \ "@mid").text)
+    return new MatchStats((match_ \ "@mid").text.toInt, match_.toString);
+  }
+
+  private def fetchMatchStats(ids: List[Int]): List[MatchStats] = {
+    println("Left: " + ids.size)
     if (ids.size == 0)
       return Nil
 
     val qids = ids.take(1)
     val query = qids.mkString("&mid[]=");
     val xmlData = XML.load(XMLRequester + "?f=match_stats&opt=mid&mid[]=" + query)
-    val ret = (for { match_ <- (xmlData \\ "match") } yield new MatchStats((match_ \ "@mid").text.toInt, match_.toString)).toList;
+    val ret = (for { match_ <- (xmlData \ "stats" \ "match") } yield new MatchStats((match_ \ "@mid").text.toInt, match_.toString)).toList;
 
+    assert(ret.size > 0)
     ret.foreach(m => m.cacheEntry(connection))
-    return ret ::: fetchMachStats(ids.drop(1))
+    return ret ::: fetchMatchStats(ids.drop(1))
   }
 
   def dispose = {
     connection.close
+    this ! "EXIT"
+  }
+
+  case class QueryArgs(query: String)
+  case class Result(stats: List[MatchStats])
+  var inProgress = 0
+  var m_results: List[MatchStats] = Nil
+
+  def act {
+    loop {
+      react {
+        case res: Result => {
+          m_results = m_results ::: res.stats
+        }
+        case "STOP" => {
+          inProgress -= 1
+        }
+        case "EXIT" => exit
+      }
+    }
+  }
+
+  class WorkerActor extends Actor {
+    def act() {
+      loop {
+        react {
+          case "STOP" => {
+            StatsFactory ! "STOP"
+            exit
+          }
+          case msg: QueryArgs => {
+            val xmlData = XML.load(msg.query)
+            val result = (for { match_ <- (xmlData \ "stats" \ "match") } yield new MatchStats((match_ \ "@mid").text.toInt, match_.toString)).toList
+            StatsFactory ! Result(result)
+          }
+        }
+      }
+    }
   }
 }
 
