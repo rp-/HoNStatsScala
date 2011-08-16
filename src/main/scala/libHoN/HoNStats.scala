c2455e99583f5b400e74e01fbb621050a0b15592
Peinthor Rene <peinthor@gmail.com>
2011-08-16 09:20:12 +0200

    use functions for output player and matches

--- src/main/scala/libHoN/HoNStats.scala
+++ src/main/scala/libHoN/HoNStats.scala
@@ -37,80 +37,10 @@ object HoNStats extends App {
 
           c.command match {
             case "player" => {
-              val players = StatsFactory.getPlayerStatsByNick(c.nicks)
-
-              val sHdOutput = "%-10s %-5s %-5s %-4s %-4s %-5s %4s %s"
-              val sPlOutput = "%-10s %-5d %4d/%4d/%4d %5.2f  %4d %d"
-              c.statstype match {
-                case "ranked" =>
-                  println(sHdOutput.format("Nick", "MMR", "K", "D", "A", "KDR", "MGP", "AID"))
-                  players.foreach(p =>
-                    println(sPlOutput.format(
-                      p.attribute(PlayerAttr.NICKNAME),
-                      p.attribute(PlayerAttr.RANK_AMM_TEAM_RATING).toFloat.toInt,
-                      p.attribute(PlayerAttr.RANK_HEROKILLS).toInt,
-                      p.attribute(PlayerAttr.RANK_DEATHS).toInt,
-                      p.attribute(PlayerAttr.RANK_HEROASSISTS).toInt,
-                      (p.attribute(PlayerAttr.RANK_HEROKILLS).toFloat / p.attribute(PlayerAttr.RANK_DEATHS).toFloat),
-                      p.attribute(PlayerAttr.RANK_GAMES_PLAYED).toInt,
-                      p.getAID.toInt)))
-                case "public" =>
-                  println(sHdOutput.format("Nick", "MMR", "K", "D", "A", "KDR", "GP", "AID"))
-
-                  players.foreach(p =>
-                    println(sPlOutput.format(
-                      p.attribute(PlayerAttr.NICKNAME),
-                      p.attribute(PlayerAttr.SKILL).toFloat.toInt,
-                      p.attribute(PlayerAttr.HEROKILLS).toInt,
-                      p.attribute(PlayerAttr.DEATHS).toInt,
-                      p.attribute(PlayerAttr.HEROASSISTS).toInt,
-                      (p.attribute(PlayerAttr.HEROKILLS).toFloat / p.attribute(PlayerAttr.DEATHS).toFloat),
-                      p.attribute(PlayerAttr.GAMES_PLAYED).toInt,
-                      p.getAID.toInt)))
-                case "casual" =>
-                  println(sHdOutput.format("Nick", "MMR", "K", "D", "A", "KDR", "CGP", "AID"))
-
-                  players.foreach(p =>
-                    println(sPlOutput.format(
-                      p.attribute(PlayerAttr.NICKNAME),
-                      p.attribute(PlayerAttr.CS_AMM_TEAM_RATING).toFloat.toInt,
-                      p.attribute(PlayerAttr.CS_HEROKILLS).toInt,
-                      p.attribute(PlayerAttr.CS_DEATHS).toInt,
-                      p.attribute(PlayerAttr.CS_HEROASSISTS).toInt,
-                      (p.attribute(PlayerAttr.CS_HEROKILLS).toFloat / p.attribute(PlayerAttr.CS_DEATHS).toFloat),
-                      p.attribute(PlayerAttr.CS_GAMES_PLAYED).toInt,
-                      p.getAID.toInt)))
-              }
+              outputPlayer(c)
             }
             case "matches" => {
-              val players = StatsFactory.getPlayerStatsByNickCached(c.nicks)
-
-              for (player <- players) {
-                val matches = player.getPlayedMatches(c.statstype, c.limit)
-
-                println(player.attribute(PlayerAttr.NICKNAME))
-                val showmatches = matches.reverse.take(c.limit)
-                println(" %-9s %-5s %-16s  %2s %2s %2s  %4s %s %s %3s/%2s %s".format(
-                  "MID", "GD", "Date", "K", "D", "A", "Hero", "W/L", "Wards", "CK", "CD", "GPM"))
-                for (outmatch <- showmatches) {
-                  Log.debug(outmatch.getMatchID.toString)
-                  val game_mins: Int = outmatch.getMatchStatAsInt(MatchAttr.TIME_PLAYED) / 60
-                  val gpm = if (game_mins > 0) outmatch.getPlayerMatchStatAsInt(player.getAID, MatchPlayerAttr.GOLD) / game_mins else 0
-                  println(" %-9d %5s %-16s  %2d/%2d/%2d  %-4s %-3s %5s %3d/%2d %3d".format(
-                    outmatch.getMatchID,
-                    outmatch.getGameDuration,
-                    dateFormat.format(outmatch.getLocalMatchDateTime),
-                    outmatch.getPlayerMatchStatAsInt(player.getAID, "herokills"),
-                    outmatch.getPlayerMatchStatAsInt(player.getAID, "deaths"),
-                    outmatch.getPlayerMatchStatAsInt(player.getAID, "heroassists"),
-                    HeroAttr.getNick(outmatch.getPlayerMatchStat(player.getAID, "hero_id").toInt),
-                    if (outmatch.playerWon(player.getAID)) "W" else "L",
-                    outmatch.getPlayerMatchStat(player.getAID, MatchPlayerAttr.WARDS),
-                    outmatch.getPlayerMatchStatAsInt(player.getAID, MatchPlayerAttr.TEAMCREEPKILLS) + outmatch.getPlayerMatchStatAsInt(player.getAID, MatchPlayerAttr.NEUTRALCREEPKILLS),
-                    outmatch.getPlayerMatchStatAsInt(player.getAID, MatchPlayerAttr.DENIES),
-                    gpm))
-                }
-              }
+              outputMatches(c)
             }
             case x => {
               println("Unknown command: '" + x + "'")
@@ -122,5 +52,83 @@ object HoNStats extends App {
     }
   } finally {
     StatsFactory.dispose
+  }
+
+  def outputPlayer(config: Configuration) = {
+    val players = StatsFactory.getPlayerStatsByNick(config.nicks)
+
+    val sHdOutput = "%-10s %-5s %-5s %-4s %-4s %-5s %4s %s"
+    val sPlOutput = "%-10s %-5d %4d/%4d/%4d %5.2f  %4d %d"
+    config.statstype match {
+      case "ranked" =>
+        println(sHdOutput.format("Nick", "MMR", "K", "D", "A", "KDR", "MGP", "AID"))
+        players.foreach(p =>
+          println(sPlOutput.format(
+            p.attribute(PlayerAttr.NICKNAME),
+            p.attribute(PlayerAttr.RANK_AMM_TEAM_RATING).toFloat.toInt,
+            p.attribute(PlayerAttr.RANK_HEROKILLS).toInt,
+            p.attribute(PlayerAttr.RANK_DEATHS).toInt,
+            p.attribute(PlayerAttr.RANK_HEROASSISTS).toInt,
+            (p.attribute(PlayerAttr.RANK_HEROKILLS).toFloat / p.attribute(PlayerAttr.RANK_DEATHS).toFloat),
+            p.attribute(PlayerAttr.RANK_GAMES_PLAYED).toInt,
+            p.getAID.toInt)))
+      case "public" =>
+        println(sHdOutput.format("Nick", "MMR", "K", "D", "A", "KDR", "GP", "AID"))
+
+        players.foreach(p =>
+          println(sPlOutput.format(
+            p.attribute(PlayerAttr.NICKNAME),
+            p.attribute(PlayerAttr.SKILL).toFloat.toInt,
+            p.attribute(PlayerAttr.HEROKILLS).toInt,
+            p.attribute(PlayerAttr.DEATHS).toInt,
+            p.attribute(PlayerAttr.HEROASSISTS).toInt,
+            (p.attribute(PlayerAttr.HEROKILLS).toFloat / p.attribute(PlayerAttr.DEATHS).toFloat),
+            p.attribute(PlayerAttr.GAMES_PLAYED).toInt,
+            p.getAID.toInt)))
+      case "casual" =>
+        println(sHdOutput.format("Nick", "MMR", "K", "D", "A", "KDR", "CGP", "AID"))
+
+        players.foreach(p =>
+          println(sPlOutput.format(
+            p.attribute(PlayerAttr.NICKNAME),
+            p.attribute(PlayerAttr.CS_AMM_TEAM_RATING).toFloat.toInt,
+            p.attribute(PlayerAttr.CS_HEROKILLS).toInt,
+            p.attribute(PlayerAttr.CS_DEATHS).toInt,
+            p.attribute(PlayerAttr.CS_HEROASSISTS).toInt,
+            (p.attribute(PlayerAttr.CS_HEROKILLS).toFloat / p.attribute(PlayerAttr.CS_DEATHS).toFloat),
+            p.attribute(PlayerAttr.CS_GAMES_PLAYED).toInt,
+            p.getAID.toInt)))
+    }
+  }
+
+  def outputMatches(config: Configuration) = {
+    val players = StatsFactory.getPlayerStatsByNickCached(config.nicks)
+
+    for (player <- players) {
+      val matches = player.getPlayedMatches(config.statstype, config.limit)
+
+      println(player.attribute(PlayerAttr.NICKNAME))
+      val showmatches = matches.reverse.take(config.limit)
+      println(" %-9s %-5s %-16s  %2s %2s %2s  %4s %s %s %3s/%2s %s".format(
+        "MID", "GD", "Date", "K", "D", "A", "Hero", "W/L", "Wards", "CK", "CD", "GPM"))
+      for (outmatch <- showmatches) {
+        Log.debug(outmatch.getMatchID.toString)
+        val game_mins: Int = outmatch.getMatchStatAsInt(MatchAttr.TIME_PLAYED) / 60
+        val gpm = if (game_mins > 0) outmatch.getPlayerMatchStatAsInt(player.getAID, MatchPlayerAttr.GOLD) / game_mins else 0
+        println(" %-9d %5s %-16s  %2d/%2d/%2d  %-4s %-3s %5s %3d/%2d %3d".format(
+          outmatch.getMatchID,
+          outmatch.getGameDuration,
+          dateFormat.format(outmatch.getLocalMatchDateTime),
+          outmatch.getPlayerMatchStatAsInt(player.getAID, "herokills"),
+          outmatch.getPlayerMatchStatAsInt(player.getAID, "deaths"),
+          outmatch.getPlayerMatchStatAsInt(player.getAID, "heroassists"),
+          HeroAttr.getNick(outmatch.getPlayerMatchStat(player.getAID, "hero_id").toInt),
+          if (outmatch.playerWon(player.getAID)) "W" else "L",
+          outmatch.getPlayerMatchStat(player.getAID, MatchPlayerAttr.WARDS),
+          outmatch.getPlayerMatchStatAsInt(player.getAID, MatchPlayerAttr.TEAMCREEPKILLS) + outmatch.getPlayerMatchStatAsInt(player.getAID, MatchPlayerAttr.NEUTRALCREEPKILLS),
+          outmatch.getPlayerMatchStatAsInt(player.getAID, MatchPlayerAttr.DENIES),
+          gpm))
+      }
+    }
   }
 }
